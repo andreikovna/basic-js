791f3e797d1ab0c500264986036e83784cdb4cb1
andreikovna <d.andreikovna@gmail.com>
2021-09-10 22:59:27 +0300

    fix: chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,27 +5,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
-  let array =[];
+  array: [],
   getLength() {
     return this.array.length
   },
   addLink(value) {
-   this.array.push(value);
+   this.array.push(`( ${value} )`);
    return this;
   },
-  
+
   removeLink(position) {
     if (typeof position !== 'number'){
-      this.arr = [];
+      this.array = [];
       throw new Error(`You can't remove incorrect link!`);
-    } else if (position > this.arr.length) {
-      this.arr = [];
+    } else if (position > this.array.length) {
+      this.array = [];
       throw new Error(`You can't remove incorrect link!`);
     } else if (position <= 0){
-      this.arr = [];
+      this.array = [];
       throw new Error(`You can't remove incorrect link!`);
     } else
-    this.arr.splice(position - 1, 1);
+    this.array.splice(position - 1, 1);
     return this;
   },
 
