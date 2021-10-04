dd170ab6364f18bc675cbd719feb267cb08beaa7
Daria Litvinova <d.andreikovna@gmail.com>
2021-10-04 23:54:54 +0300

    feat: add mine solution

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,48 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper (matrix) {
+  let mine = new Array(matrix.length);
+let result = new Array(matrix.length);
+
+for (let i = 0; i < mine.length; i++) {
+  mine[i] = new Array(matrix[0].length);
+	result[i] = new Array(matrix[0].length);
+}
+
+for (let i=0; i < matrix.length; i++) {
+	for (let j=0; j < matrix[i].length; j++){
+		if (matrix[i][j] == true) {
+			mine[i][j] = 1;
+			
+		} else {
+			mine[i][j] = 0;
+		}
+	}
+}
+
+for (let i=0; i<mine.length; i++){
+	for (let j=0; j<mine[i].length; j++) {
+		if (i == 0 & j == 0){
+			result[i][j] = mine[i][j+1]+mine[i+1][j]+mine[i+1][j+1]
+		} else if ( i==0 & j == mine[i].length-1) {
+			result[i][j] = mine[i][j-1]+mine[i+1][j]+mine[i+1][j-1]
+		} else if (i==0){
+		result[i][j] = mine[i][j-1]+mine[i][j+1]+mine[i+1][j-1]+mine[i+1][j+1] + mine[i+1][j]
+	} else if (i==mine.length-1 & j == 0){
+	result[i][j] = mine[i-1][j]+mine[i-1][j+1]+mine[i][j+1]
+	} else if (i==mine.length-1 & j==mine[i].length-1){
+result[i][j] = mine[i-1][j]+mine[i-1][j-1]+mine[i][j-1]
+	} else if (i==mine.length-1) {
+result[i][j] = 	mine[i][j-1]+mine[i][j+1]+mine[i-1][j-1]+mine[i-1][j+1]+mine[i-1][j]
+	} else if (j==0){
+result[i][j] = mine[i-1][j]+mine[i-1][j+1]+mine[i][j+1]+mine[i+1][j+1]+mine[i+1][j]		
+	} else if (j==mine[i].length-1){
+result[i][j] = mine[i-1][j]+mine[i-1][j-1]+mine[i][j-1]+mine[i+1][j-1]+mine[i+1][j]		
+	} else {
+result[i][j] = mine[i-1][j-1]+mine[i-1][j]+mine[i-1][j+1]+mine[i][j-1]+mine[i][j+1]+mine[i+1][j-1]+mine[i+1][j]+mine[i+1][j+1]	
+	}
+}
+}
+return result;
 }
